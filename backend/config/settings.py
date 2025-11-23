ba52a2809589c4a81adb8651b66fe49bc207d257
Nail Razakov Win10 <nailrazakov1982@gmail.com>
2025-11-23 23:21:50 +0300

    Frontend: Создание приложения на Vue JS с компонентами, добавление  порта 5173 в CORS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -42,9 +42,10 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "http://127.0.0.1:8080",
-    "http://frontend:8080",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://frontend:8000",
+    "http://localhost:5173",
 ]
 
 MIDDLEWARE = [
